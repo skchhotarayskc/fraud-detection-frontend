0e5821ae7d68eaf71179c9129e768f4f5be9c195
Sushant <skchhotarayskc@gmail.com>
2023-09-01 15:14:17 +0530

    adding react-router-dom and route the files

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    user_id: '',
+    email: '',
+    password: '',
+    otp: '',
+  });
+
+  const [showOtpSection, setShowOtpSection] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handlePasswordSubmit = (e) => {
+    e.preventDefault();
+    setShowOtpSection(true);
+  };
+
+  const handleOtpSubmit = (e) => {
+    e.preventDefault();
+    alert('OTP Submitted');
+  };
+
   return (
     <div>
-      
-    </div>
-  )
-}
+      <form onSubmit={handlePasswordSubmit}>
+        <div>
+          <label>User ID:</label>
+          <input
+            type="text"
+            name="user_id"
+            value={formData.user_id}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Send OTP</button>
+      </form>
 
-export default Login
+      {showOtpSection && (
+        <form onSubmit={handleOtpSubmit}>
+          <div>
+            <label>Enter OTP:</label>
+            <input
+              type="text"
+              name="otp"
+              value={formData.otp}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit">Submit OTP</button>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default Login;
